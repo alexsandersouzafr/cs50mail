4ac112fbfbcd384f11ca4c7804097888de8c2334
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2022-04-04 08:45:59 -0300

    first commit

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+
+  document.querySelector('#compose-form').onsubmit = function() {
+    alert('aeee otário')
+  }
+
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // TODO:GET e-mails
+  fetch(`emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    document.getElementById('#emails-view').appendChild(emails)
+    });
+}
+
